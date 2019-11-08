d55fe197c2c66421480dc9d16160afc827f83fc7
Luca Alexandru <luca.p.alexandru@gmail.com>
2019-11-08 16:50:43 +0200

    add typings; use barrel export

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Router_1 = require("router/Router");
+exports.Router = Router_1.Router;
+//# sourceMappingURL=index.js.map
---
+++ dist/router/chain-middleware.d.ts
@@ -0,0 +1,1 @@
+export declare function chainMiddleware(series: any): (req: any, res: any, next?: any) => Promise<unknown>;
